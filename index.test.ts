2f45216d216d80edbec69b36eb5f2fd0df6de207
Jason Raimondi <jason@raimondi.us>
2024-03-11 23:53:43 -0400

    feat: working browser encrypt/decrypt

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,5 @@
-import {assertEquals} from "https://deno.land/std/testing/asserts.ts";
-import {StringEncrypter} from "./index.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { StringEncrypter } from "./index.ts";
 
 Deno.test("StringEncrypter class", async (t) => {
   const secret = "my-secret-key";
@@ -11,14 +11,4 @@ Deno.test("StringEncrypter class", async (t) => {
     const decryptedText = await crypto.decrypt(encryptedText);
     assertEquals(decryptedText, clearText);
   });
-
-  // await t.step("should throw an error when decrypting with the wrong secret", async () => {
-  //   const clearText = "Hello, World!";
-  //   const encryptedText = await crypto.encrypt(clearText);
-  //   const wrongPasswordEncrypter = new StringEncrypter("wrong-secret");
-  //   await assertEquals(
-  //     wrongPasswordEncrypter.decrypt(encryptedText),
-  //     Promise.reject(TypeError)
-  //   );
-  // });
 });
--- index.ts
+++ index.ts
@@ -1,25 +1,41 @@
 export class StringEncrypter {
-  constructor(private readonly secret: string, private cryptoKey?: CryptoKey) {}
+  constructor(private readonly secret: string, private cryptoKey?: CryptoKey) {
+  }
 
-  async getCryptoKey(): Promise<CryptoKey> {
+  async encrypt(clearText: string): Promise<ArrayBuffer> {
+    const key = await this.getCryptoKey();
+    return await window.crypto.subtle.encrypt(
+      this.algo,
+      key,
+      new TextEncoder().encode(clearText),
+    );
+  }
+
+  async decrypt(encryptedText: ArrayBuffer): Promise<string> {
+    const key = await this.getCryptoKey();
+    const decrypted = await window.crypto.subtle.decrypt(
+      this.algo,
+      key,
+      encryptedText,
+    );
+    return new TextDecoder().decode(decrypted);
+  }
+
+  private async getCryptoKey(): Promise<CryptoKey> {
     if (!this.cryptoKey) {
-      this.cryptoKey = await window.crypto.subtle.generateKey({
-        name: "AES-GCM",
-        length: 256
-      }, false, ["encrypt", "decrypt"]);
+      this.cryptoKey = await window.crypto.subtle.generateKey(
+        {
+          name: "AES-GCM",
+          length: 256,
+        },
+        false,
+        ["encrypt", "decrypt"],
+      );
     }
     return this.cryptoKey;
   }
 
-  async encrypt(clearText: string): Promise<string> {
-    const key = await this.getCryptoKey();
-    console.log(key);
-    return clearText;
-  }
-
-  async decrypt(encryptedText: string): Promise<string> {
-    const key = await this.getCryptoKey();
-    console.log(key);
-    return encryptedText;
+  private get algo() {
+    return { name: "AES-GCM", iv: new Uint8Array(12) };
   }
 }
